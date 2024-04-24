cf09d8a744cdc4971bc2b93712308f90c2f95964
Huzaifa164 <ansarihuzaifa654321@gmail.com>
2024-04-24 19:54:36 +0530

    arrays and loops exercises completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,5 @@
-const reverseString = function() {
-
+const reverseString = function(val) {
+    return val.split('').reverse().join('');
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if(num1 < 0 || num2 < 0 || typeof num1 !== "number" || typeof num2 !== "number") {
+        return "ERROR";
+    }
+    let count = 0;
+    let fi = Math.min(num1, num2);
+    let li = Math.max(num1, num2);
+    for(let i = fi; i <= li; i++) {
+        count += i;
+    }
+    return count;
 };
 
 // Do not edit below this line
